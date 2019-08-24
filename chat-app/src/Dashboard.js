6d22f6fa6b5f1ef289d93a9e0e44dafb7a30e298
Nicholas Loperena <nicholasloperena@gmail.com>
2019-08-23 23:37:40 -0400

    front-end is pretty much done, and now it's time to work with sockets.io

--- chat-app/src/Dashboard.js
+++ chat-app/src/Dashboard.js
@@ -43,60 +43,64 @@ export default function Dashboard() {
   const classes = useStyles();
 
     //Context Store
+    const [allChats] = React.useContext(CTX);
+    const topics = Object.keys(allChats);
 
-  const [textValue, changeTextValue] = React.useState('');
+    //local state
+    const [activeTopic, changeActiveTopic] = React.useState(topics[0])
+    const [textValue, changeTextValue] = React.useState('');
 
-  return (
-    <div>
-      <Paper className={classes.root}>
-        <Typography variant="h4" component="h4">
-          Chat App
-        </Typography>
-        <Typography variant= "h5" component="h5">
-          Topic Placeholder
-        </Typography>
-        <div className = {classes.flex}>
-            <div className = {classes.topicsWindow}>
-                <List>
+    return (
+        <div>
+        <Paper className={classes.root}>
+            <Typography variant="h4" component="h4">
+            Chat App
+            </Typography>
+            <Typography variant= "h5" component="h5">
+            {activeTopic}
+            </Typography>
+            <div className = {classes.flex}>
+                <div className = {classes.topicsWindow}>
+                    <List>
+                        {
+                            topics.map(topic => (
+                                <ListItem onClick={e => changeActiveTopic(e.target.innerText)} key = { topic } button>
+                                    <ListItemText primary = { topic } />
+                                </ListItem>
+                            ))
+                        }
+                    </List>
+                </div>
+
+            <div className = {classes.chatWindow}>
                     {
-                        ['topic'].map(topic => (
-                            <ListItem key = { topic } button>
-                                <ListItemText primary = { topic } />
-                            </ListItem>
+                        allChats[activeTopic].map((chat, i) => (
+                            <div className ={classes.flex} key={i}>
+                                <Chip label={chat.from} className={classes.chip} />
+                                <Typography variant = 'body1'>{chat.msg}</Typography>
+                            </div>
                         ))
                     }
-                </List>
-            </div>
+                    
+                </div>
+                <div className = {classes.chatWindow}>
 
-        <div className = {classes.chatWindow}>
-                {
-                    [{from: 'user', msg: 'hello'}].map((chat, i) => (
-                        <div className ={classes.flex} key={i}>
-                            <Chip label={chat.from} className={classes.chip} />
-                            <Typography variant = 'p'>{chat.msg}</Typography>
-                        </div>
-                    ))
-                }
+                </div>
+            </div>
+            <div className = {classes.flex}>
+                        <TextField
+                            label="Send a message"
+                            className={classes.chatBox}
+                            value={textValue}
+                            onChange={e => changeTextValue(e.target.value)}
+                        />
+
                 
+                        <Button color="primary" className={classes.button}>
+                            Send
+                </Button>
             </div>
-            <div className = {classes.chatWindow}>
-
-            </div>
+        </Paper>
         </div>
-        <div className = {classes.flex}>
-                    <TextField
-                        label="Send a message"
-                        className={classes.chatBox}
-                        value={textValue}
-                        onChange={e => changeTextValue(e.target.value)}
-                    />
-
-            
-                    <Button color="primary" className={classes.button}>
-                        Send
-            </Button>
-        </div>
-      </Paper>
-    </div>
-  );
+    );
 }
